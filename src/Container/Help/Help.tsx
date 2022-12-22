6dacbd304bc47aa4bf4476a320ed29bf8023d2ed
Al Yusra Sikander <112174116+codewithyusra@users.noreply.github.com>
2022-12-22 20:38:34 +0530

    alyus help first iteration

--- src/Container/Help/Help.tsx
+++ src/Container/Help/Help.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import './help.css';
+import { borderRadius } from '@mui/system';
+import * as React from 'react'
+
 import Sidebar from '../../components/sidebar/Sidebar';
+import HelpModal from './HelpModal';
 
 export default function Help() {
-  return (
-    <div>
-      <Sidebar/>
-      <div className='htext1'>Help</div>
-      <div className='htext2'>Read More</div>
-      <div className='htext3'>Privacy Policy</div>
-      <div className='htext4'>Support</div>
-      <div className='htext5'>Terms of Service</div>
-      <div className='htext6'>FAQs</div>
 
-      <button className='hrect1'></button>
-      <button className='hrect2'></button>
-      <button className='hrect3'></button>
-      <button className='hrect4'></button>
-      <button className='hrect5'></button>
-      <div className='hrect6'></div>
+
+  return (
+    <div style={{ backgroundColor: '#959595' }}>
+      <Sidebar />
+      <div style={{marginLeft:"25%",marginTop:"10%" ,marginRight:"5%",padding :'20px,30px,20px,30px',textAlign:"left",}}> 
+      <div style={{fontFamily:"bold", fontSize:"28px",}}>HELP
+      <HelpModal Text={"Read More"} /> 
+      <HelpModal Text={"Privacy Policy"} />
+      <HelpModal Text={"Support" }/>
+      <HelpModal Text={"Terms of Services" }/>
+      <HelpModal Text={"FAQs" }/>
+      </div>
+
+
+    </div>
     </div>
   )
 }
---
+++ src/Container/Help/HelpModal.tsx
@@ -0,0 +1,136 @@
+// import React from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import Modal from '@mui/material/Modal';
+
+
+
+//  function HelpModal(show:any ,hide:any,isOpen:boolean) {
+    
+    
+//     return (
+//         <div>
+//             <Button onClick={show.show}>Open modal</Button>
+//             <Modal
+//                 open={show.isOpen}
+//                 onClose={show.hide}
+//                 aria-labelledby="modal-modal-title"
+//                 aria-describedby="modal-modal-description"
+//             >
+//                 <Box>
+//                     <Typography id="modal-modal-title" variant="h6" component="h2">
+//                         Text in a modal
+//                     </Typography>
+//                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+//                         Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//                     </Typography>
+//                 </Box>
+//             </Modal>
+//         </div>
+//     );
+// }
+
+// export default HelpModal
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import './help.css';
+import { OutlinedInput } from '@mui/material';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+export interface DialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
+
+function BootstrapDialogTitle(props: DialogTitleProps) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 ,}} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+export default function HelpModal( Text:any) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+   console.log(Text.Text);
+   
+  return (
+    <div style={{background:"white",marginTop:"3%",padding:"1%",  border: "white" ,borderRadius:"20px"}}>
+      <Button sx={{width:"100%", color:"black", textAlign:"left",fontSize:"bold",justifyContent:"left"}}  onClick={handleClickOpen}>
+       {Text.Text}
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Modal title
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+            ullamcorper nulla non metus auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </div>
+  );
+}
